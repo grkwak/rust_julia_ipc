d8787462d94ef6325ab6f2cc566eb980db53bb77
Grace Kwak <grace.kwak@flyzipline.com>
2024-06-20 08:14:00 -0700

    Try using mio but getting error with Poll:new()

--- src/main.jl
+++ src/main.jl
@@ -1,9 +1,19 @@
-function call_rust_function(function_name::String)
-    open(`./path_to_your_rust_program`, "w+", stdout) do io
-        println(io, function_name)
-        return readline(io)
+function main()
+    pipe = open("~/Desktop/rust_julia_ipc/", "r+")
+    println("Begin")
+    while true
+        sleep(1)
+
+        println(pipe, "get_bytes1") # These get_bytess do not use standard IO; instead, they use the IO stream associated with the pipe.
+        flush(pipe)
+        result = readline(pipe) # Blocks until a \n is received
+        println("Received get_bytes_1: ", result)
+
+        println(pipe, "get_bytes2") # These get_bytess do not use standard IO; instead, they use the IO stream associated with the pipe.
+        flush(pipe)
+        result = readline(pipe) # Blocks until a \n is received
+        println("Received get_bytes_2: ", result)
     end
 end
 
-println(call_rust_function("function1"))
-println(call_rust_function("function2"))
+main()
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,61 @@
-use std::io::{self, BufRead, Write};
+// use std::fs::OpenOptions;
+use std::io::{BufRead, BufReader, Write};
+use mio::{Events, Interest, Poll, Token};
+// use mio::unix::SourceFd;
+// use std::os::unix::io::AsRawFd;
 
 fn main() {
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        let line = line.unwrap();
-        match line.as_str() {
-            "function1" => {
-                let result = function1();
-                writeln!(io::stdout(), "{}", result).unwrap();
-            }
-            "function2" => {
-                let result = function2();
-                writeln!(io::stdout(), "{}", result).unwrap();
-            }
-            _ => {
-                writeln!(io::stderr(), "Unknown function: {}", line).unwrap();
-            }
-        }
-    }
+    let pipe = OpenOptions::new().read(true).write(true).open("Users/grace.kwak/Desktop/rust_julia_ipc/").unwrap();
+    let reader = BufReader::new(&pipe);
+
+    let mut poll = Poll::new().unwrap();
+
+    // let mut poll = Poll::new();//.unwrap();
+    // let mut events = Events::with_capacity(1);
+
+    // // Register our `pipe` with the `Poll` instance.
+    // poll.registry().register(&pipe, Token(0), Interest::READABLE).unwrap(); //maybe &mut pipe
+
+    // loop { // This is an infinite loop that keeps the program running until it's manually stopped
+    //     poll.poll(&mut events, None).unwrap(); // This line blocks the program and waits for events. When an event occurs, it will return and we can handle the event
+
+    //     for event in events.iter() { // there should only be 1 event
+    //         match event.token() {
+    //             Token(0) => { // If the token is 0, it means the event came from the source registered with token 0 (in this case, the pipe).
+    //                 for line in reader.lines() { //there should only be 1 line
+    //                     let line = line.unwrap();
+    //                     match line.as_str() {
+    //                         "get_bytes_1" => {
+    //                             let result = get_bytes_1();
+    //                             let slice: &[u8] = &result;
+    //                             pipe.write_all(slice).unwrap();
+    //                             // writeln!(pipe, "{}", result).unwrap();
+    //                         }
+    //                         "get_bytes_2" => {
+    //                             let result = get_bytes_2();
+    //                             let slice: &[u8] = &result;
+    //                             pipe.write_all(slice).unwrap();
+    //                             // writeln!(pipe, "{}", result).unwrap();
+    //                         }
+    //                         _ => {
+    //                             // writeln!(pipe, "Unknown function: {}", line).unwrap();
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //             _ => unreachable!(),
+    //         }
+    //     }
+    // }
 }
 
-fn function1() -> i32 {
-    // Your function implementation here
-    42
+/*
+If you want to return a byte slice that isn't static (i.e., it doesn't live for the entire duration of the program), you'll need to return a Vec<u8> instead, or use lifetimes to ensure the data the returned slice references lives long enough. Here's an example with Vec<u8>:
+*/
+fn get_bytes_1() -> Vec<u8> {
+    vec![1, 2, 3]
 }
 
-fn function2() -> i32 {
-    // Your function implementation here
-    24
+fn get_bytes_2() -> Vec<u8> {
+    vec![4, 5, 6]
 }
