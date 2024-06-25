03ecbbf0f211cc8cea552a957ea2c177228a7e3d
Grace Kwak <grace.kwak@flyzipline.com>
2024-06-25 10:21:30 -0700

    Separate calling Rust step() vs init() or close()

--- src/julia_sim.jl
+++ src/julia_sim.jl
@@ -4,10 +4,10 @@ Yes, the read function in Julia is a blocking operation. If there aren't enough 
 In your code, read(pipe, 10) will block until 10 bytes are available to read from the pipe. If fewer than 10 bytes are available, your program will pause and wait until more bytes are written to the pipe.
 =#
 
-# func_name = { init, step, close}
-function call_rust_func(func_name::String, num::Float64)
+# func_name = { init, close}
+function call_init_or_close(func_name::String, num::Float64)
 
-    println("\n\nJULIA: Begin call_rust_func")
+    println("\n\nJULIA: Begin calling ", func_name, "()")
 
     outgoing_pipe = open("j2r_" * func_name, "w")
 
@@ -26,14 +26,40 @@ function call_rust_func(func_name::String, num::Float64)
 
 end
 
+function call_step(num::Float64, outgoing_pipe, incoming_pipe)
+
+    println("\n\nJULIA: Begin calling step()")
+
+    # Write to the pipe
+    bytes = reinterpret(UInt8, [num])
+    write(outgoing_pipe, bytes) # These get_bytes do not use standard IO; instead, they use the IO stream associated with the pipe.
+    flush(outgoing_pipe)
+    println("JULIA: Waiting for response")
+
+    # Wait for a certain character to be pushed to the pipe
+    result = read(incoming_pipe, 8) # Blocks until 8 bytes are received
+    println("JULIA: Received Rust result ", reinterpret(Float64, result)) # First want to know that Rust finished stopping step()
+
+    if isnan(num)
+        close(outgoing_pipe)
+        close(incoming_pipe)
+        return
+    end
+
+end
+
 function main()
     println("JULIA: Begin main\n\n")
 
-    call_rust_func("init", 1.0)
-    call_rust_func("step", 2.0)
-    call_rust_func("step", 3.0)
-    call_rust_func("step", 999) # the special character that tells Rust we're done
-    call_rust_func("close", 5.0)
+    call_init_or_close("init", 1.0)
+
+    outgoing_pipe = open("j2r_step", "w") # Open a pipe for writing
+    incoming_pipe = open("r2j_step", "r") # Open a pipe for reading
+    call_step(2.0, outgoing_pipe, incoming_pipe)
+    call_step(3.0, outgoing_pipe, incoming_pipe)
+    call_step(NaN, outgoing_pipe, incoming_pipe) # the special character that tells Rust we're done
+    # Maybe when close() is called, that's when the step pipes should be closed
+    call_init_or_close("close", 5.0)
 
     println("\n\nJULIA: End main")
 end
--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,9 @@ fn main() -> std::io::Result<()> {
     println!("RUST: Begin main\n\n");
 
     // let mut str = "init"; //TODO: only have 1 incoming pipe and in that incoming pipe, say which Rust function to call AND the necessary params
-    wait_for_julia_call("init")?;
-    wait_for_julia_call("step")?; // won't actually know how many times Rust will be called
-    wait_for_julia_call("close")?;
+    handle_init_or_close("init")?;
+    handle_step()?; // won't actually know how many times Rust will be called
+    handle_init_or_close("close")?;
 
     println!("\n\nRUST: End main");
 
@@ -19,17 +19,14 @@ fn main() -> std::io::Result<()> {
 }
 
 // func_name: { init, step, close }
-fn wait_for_julia_call(func_name: &str) -> std::io::Result<f64> {
+fn handle_init_or_close(func_name: &str) -> std::io::Result<()> {
     println!("\n\nRUST: Begin wait_for_julia_call");
 
     let outgoing_pipe_path = String::from("r2j_") + func_name;
     let incoming_pipe_path = String::from("j2r_") + func_name;
 
     // Open the named pipe for reading
-    let incoming_file = OpenOptions::new()
-        .read(true)
-        .write(false)
-        .open(incoming_pipe_path);
+    let incoming_file = OpenOptions::new().read(true).open(incoming_pipe_path);
 
     // Check if opening the incoming file was successful
     let mut incoming_file = match incoming_file {
@@ -39,25 +36,19 @@ fn wait_for_julia_call(func_name: &str) -> std::io::Result<f64> {
 
     let mut buffer = [0; 8]; // The command is 8 bytes long
     incoming_file.read_exact(&mut buffer)?; // Use `?` to propagate errors
-    let num = f64::from_le_bytes(buffer); // Convert the bytes to a float
-    println!("RUST: Received number: {}", num);
+    println!("RUST: Received number: {}", f64::from_le_bytes(buffer));
 
     let mut result = [0; 8]; // The result is 8 bytes long
     if func_name == "init" {
-        result = rust_init(num);
-    } else if func_name == "step" {
-        result = rust_step(num);
+        result = rust_init(buffer);
     } else if func_name == "close" {
-        result = rust_close(num);
+        result = rust_close(buffer);
     } else {
         panic!("Invalid function name");
     }
 
     // Open the named pipe for writing
-    let outgoing_file = OpenOptions::new()
-        .read(false)
-        .write(true)
-        .open(outgoing_pipe_path);
+    let outgoing_file = OpenOptions::new().write(true).open(outgoing_pipe_path);
 
     // Check if opening the outgoing file was successful
     let mut outgoing_file = match outgoing_file {
@@ -67,28 +58,64 @@ fn wait_for_julia_call(func_name: &str) -> std::io::Result<f64> {
 
     outgoing_file.write_all(&result[..])?;
 
-    Ok(num) // Return the number
+    Ok(()) // Return the number
 
     //Rust will close pipes for me automatically
 }
 
-fn rust_init(num: f64) -> [u8; 8] {
+fn handle_step() -> io::Result<()> {
+    let incoming_pipe_path = "j2r_step";
+    let outgoing_pipe_path = "r2j_step";
+
+    let mut incoming_pipe = OpenOptions::new().read(true).open(incoming_pipe_path)?;
+    let mut outgoing_pipe = OpenOptions::new().write(true).open(outgoing_pipe_path)?;
+
+    let mut buffer = [0; 8]; // Assuming we're working with single-byte commands for simplicity
+
+    loop {
+        println!("\n\nRUST: Begin handle_step");
+
+        match incoming_pipe.read_exact(&mut buffer) {
+            Ok(_) => {
+                let num = f64::from_le_bytes(buffer);
+                println!("RUST: Received number: {}", num);
+                if num.is_nan() {
+                    break;
+                }
+                let result = rust_step(buffer);
+                outgoing_pipe.write_all(&result[..])?;
+            }
+            Err(e) => return Err(e),
+        }
+    }
+
+    // Pipes will be automatically closed when `incoming_pipe` and `outgoing_pipe` go out of scope
+    Ok(())
+}
+
+fn rust_init(bytes: [u8; 8]) -> [u8; 8] {
+    let num = f64::from_le_bytes(bytes); // Convert the bytes to a float
     let val = num * 10.0;
-    let bytes = val.to_le_bytes(); // convert the float to little-endian bytes
     sleep(Duration::from_secs(5));
+    let bytes = val.to_le_bytes(); // convert the float to little-endian bytes
+
     bytes
 }
 
-fn rust_step(num: f64) -> [u8; 8] {
+fn rust_step(bytes: [u8; 8]) -> [u8; 8] {
+    let num = f64::from_le_bytes(bytes); // Convert the bytes to a float
     let val = num * 100.0;
-    let bytes = val.to_le_bytes(); // convert the float to little-endian bytes
     sleep(Duration::from_secs(5));
+    let bytes = val.to_le_bytes(); // convert the float to little-endian bytes
+
     bytes
 }
 
-fn rust_close(num: f64) -> [u8; 8] {
+fn rust_close(bytes: [u8; 8]) -> [u8; 8] {
+    let num = f64::from_le_bytes(bytes); // Convert the bytes to a float
     let val = num * 1000.0;
-    let bytes = val.to_le_bytes(); // convert the float to little-endian bytes
     sleep(Duration::from_secs(5));
+    let bytes = val.to_le_bytes(); // convert the float to little-endian bytes
+
     bytes
 }
